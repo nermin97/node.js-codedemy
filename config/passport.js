980a781bd299442dd1da4af238b09c5a09485b58
nerminKrdzic <nermin.krdzic@pragmatio.com>
2020-08-20 18:24:47 +0200

    mailchimp integration

--- config/passport.js
+++ config/passport.js
@@ -1,6 +1,8 @@
 var passport = require("passport");
 var FacebookStrategy = require("passport-facebook").Strategy;
 var secret = require("./secret");
+var async = require("async");
+var request = require("request");
 
 var User = require("../models/user");
 
@@ -24,18 +26,50 @@ passport.use(
           req.flash("loginMessage", "Successfully login with Facebook");
           return done(null, user);
         } else {
-          var newUser = new User();
-          newUser.email = profile._json.email;
-          newUser.email = profile.id;
-          newUser.tokens.push({ kind: "facebook", token: token });
-          newUser.profile.name = profile.displayName;
-          newUser.profile.picture =
-            "https://graph.facebook.com/" + profile.id + "/picture?type=large";
-          newUser.save((err) => {
-            if (err) throw err;
-            req.flash("loginMessage", "Successfully login with Facebook");
-            return done(null, newUser);
-          });
+          async.waterfall([
+            (callback) => {
+              var newUser = new User();
+              newUser.email = profile._json.email;
+              newUser.email = profile.id;
+              newUser.tokens.push({ kind: "facebook", token: token });
+              newUser.profile.name = profile.displayName;
+              newUser.profile.picture =
+                "https://graph.facebook.com/" +
+                profile.id +
+                "/picture?type=large";
+              newUser.save((err) => {
+                if (err) throw err;
+                req.flash("loginMessage", "Successfully login with Facebook");
+                callback(err, newUser);
+              });
+            },
+            (newUser, callback) => {
+              request(
+                {
+                  url:
+                    "https://us17.api.mailchimp.com/lists/996bf199c6/members",
+                  method: "POST",
+                  headers: {
+                    Authorization:
+                      "randomUser 4a05f84ae5b53b0b489ea9c86955b1c6-us17",
+                    "Content-Type": "application/json",
+                  },
+                  json: {
+                    email_adress: newUser.email,
+                    status: "subscribed",
+                  },
+                },
+                (err, response, body) => {
+                  if (err) {
+                    return done(err, newUser);
+                  } else {
+                    console.log("Success");
+                    return done(null, newUser);
+                  }
+                }
+              );
+            },
+          ]);
         }
       });
     }
